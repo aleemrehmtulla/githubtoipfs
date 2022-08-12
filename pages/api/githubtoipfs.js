f4fb0e0dfb5eafc73eda86f925e9acd0ad37aab3
Aleem Rehmtulla <60443878+aleemrehmtulla@users.noreply.github.com>
2022-08-11 23:32:51 -0700

    Fix API

--- pages/api/githubtoipfs.js
+++ pages/api/githubtoipfs.js
@@ -4,9 +4,18 @@ export default async function handler(req, res) {
   const repo = await fetch(req.body);
   const repoZip = await repo.arrayBuffer();
 
+  const projectId = process.env.NEXT_PUBLIC_INFURA_PROJECTID;
+  const projectSecret = process.env.NEXT_PUBLIC_INFURA_KEY;
+
   const client = create({
     host: "ipfs.infura.io",
+    port: 5001,
     protocol: "https",
+    headers: {
+      authorization:
+        "Basic " +
+        Buffer.from(projectId + ":" + projectSecret).toString("base64"),
+    },
   });
 
   const { cid } = await client.add(repoZip);
